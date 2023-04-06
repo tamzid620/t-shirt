ee5c1c7d01792bf768010c5c9d4d231c78fb1123
tamzid620 <tamzid620@gmail.com>
2023-04-06 21:31:44 +0600

    first commit

---
+++ src/Components/OrderRevies/OrderRevies.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const OrderRevies = () => {
+    return (
+        <div>
+            <h1>This is order section </h1>
+        </div>
+    );
+};
+
+export default OrderRevies;
---
+++ src/main.jsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
+import './index.css'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import OrderRevies from './Components/OrderRevies/OrderRevies';
+// routes section ---------------------------
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Main></Main>,
+    children:[
+      {
+        path:"/",
+        element: <Home></Home>,
+        loader: ()=> fetch("tshirts.json")
+      },
+      {
+        path: "review",
+        element: <OrderRevies></OrderRevies>
+      }
+    ]
+  },
+]);
+
+
+// routes system ------------------------------
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+     <RouterProvider router={router} />
+  </React.StrictMode>,
+)
